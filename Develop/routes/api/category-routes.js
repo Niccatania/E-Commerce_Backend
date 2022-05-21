19ab42c7dedf8f5228a538d80a794602ae15ca05
Niccatania <Niccatania6@gmail.com>
2022-05-21 15:58:06 -0700

    rputes update

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,98 @@
+const router = require('express').Router();
+// const { where } = require('sequelize/types');
+const { Category, Product, ProductTag } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  try {
+    const categoryData = await Category.findAll({
+      include: {
+        model: Product,
+        
+      }
+    }) 
+res.status(200).json(categoryData);  
+} catch (err) {
+    res.status(400).json(err);
+  } 
+});
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findOne({
+      where:{id: req.params.id},
+      include: [{ model: Product
+      }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  } 
+});
+
+
+
+  
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+
+
+router.put('/:id',  (req, res) => {
+  // update a category by its `id` value
+   Category.update(
+     {
+      id: req.params.id},{
+      where: {
+        id: req.params.id
+      },
+    }
+    )
+    .then((updatedID) => {
+      res.json(updatedID);
+    })
+    .catch((err) => res.json(err));
+
+});
+
+
+
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
